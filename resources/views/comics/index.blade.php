c48b7a9ed09c7e29b6521e59e2dfc7d49865c0be
pierfrancescoscar <scarinci401@gmail.com>
2022-01-26 16:13:45 +0100

    ADD: table w/data  to archive page

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,5 +1,35 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <h1>Archive</h1>
+    <section class="container p-5">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>id</th>
+                            <th>title</th>
+                            <th>description</th>
+                            <th>price</th>
+                            <th colspan="3">action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($comics as $comic)
+                            <tr>
+                                <td>{{ $comic->id }}</td>
+                                <td>{{ $comic->title }}</td>
+                                <td class="text-truncate" style="max-width: 250px">{{ $comic->description }}</td>
+                                <td>{{ $comic->price }}</td>
+                                <td>
+                                    <a class="btn btn-success" href="/">show</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
 @endsection
