04ceb04d30a5d3f3ad9bdc9c694d81c27ff527ec
pierfrancescoscar <scarinci401@gmail.com>
2022-01-26 14:46:09 +0100

    ADD: general setup w/controller and model

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends ('layouts.main')
+
+@section('main-content')
+<section class="container-fluid">
+    <div class="container p-5">
+        <div class="row">
+            <div class="col-12">
+                <h3 class="mb-5 text-center">Comics</h3>
+            </div>
+        <div class="row justify-content-center">
+            Content
+        </div>
+        </div>
+    </div>
+</section>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- Footer -->
+<footer class="page-footer font-small blue fixed-bottom">
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3 bg-dark">
+        <span class="text-white">© 2022 Copyright:</span>
+        <span class="text-white"> Comics - Made with love by Pierfrancesco S.</span>
+    </div>
+
+</footer>
