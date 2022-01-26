9c57db9cad06fd0108d7d1cd6b1faf01ddb008b0
pierfrancescoscar <scarinci401@gmail.com>
2022-01-26 17:43:22 +0100

    ADD: some text to the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,16 @@
 @section('main-content')
 <section class="container-fluid">
     <div class="container p-5">
-        <div class="row">
-            <div class="col-12">
-                <h3 class="mb-5 text-center">Comics List</h3>
+            <div class="row">
+                <div class="col-12">
+                    <h3 class="mb-5 text-center">Comics List</h3>
+                </div>
+            <div class="row text-center">
+                <h4>There are a lot of comics here.
+                    Click on the button to see all of them.
+                </h4>
+                <a href="{{route('comics.index')}}">Click here</a>
             </div>
-        <div class="row justify-content-center">
-            Content
-        </div>
         </div>
     </div>
 </section>
